514c30b4da3ac524098f03fbabce04bd40568094
Tomas Padilla <padillatomasagustin@gmail.com>
2025-01-10 13:31:32 +0100

    [FE-6] Crear rama y preparar para proyecto

--- docs/assets/reset_migrations.bash
+++
@@ -1,7 +0,0 @@
-rm -R -f ./migrations &&
-pipenv run init &&
-psql -U postgres -c "DROP DATABASE IF EXISTS deliveryfood";
-psql -U postgres -c "CREATE DATABASE deliveryfood";
-psql -U postgres -c "CREATE EXTENSION unaccent" -d deliveryfood;
-pipenv run migrate &&
-pipenv run upgrade
