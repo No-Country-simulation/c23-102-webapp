0f0a062a3437e4105c07e0cf24e5e125d90a1d52
Pablo Alonso <pabloalonsonext@gmail.com>
2025-01-12 12:28:55 -0300

    [FE-1] Creacion del proyecto base

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const App = () => {
+  return (
+    <div>
+      <h1>¡Hola, mundo!</h1>
+      <p>Bienvenido a tu aplicación React desde cero.</p>
+    </div>
+  );
+};
+export default App;
