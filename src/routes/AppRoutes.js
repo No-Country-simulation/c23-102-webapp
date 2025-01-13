3becf0e379b8a63b6bc21a9bbc1eb756b374c066
Tomas Padilla <padillatomasagustin@gmail.com>
2025-01-13 15:46:54 +0100

    [FE-12] Crear Homepage vacia

---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import WebsiteRoutes from "../module/website/WebsiteRoutes";
+
+const AppRoutes = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/*" element={<WebsiteRoutes />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
+
+export default AppRoutes;
