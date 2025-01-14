533599854a5659ecd45acdb45a978a181d3b9693
Tomas Padilla <padillatomasagustin@gmail.com>
2025-01-14 16:46:25 +0100

    [FE-18] Add some stuff

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,11 +1,24 @@
 import React from "react";
+import Link from "next/link";
+
 import { Topbar } from "./_components/Topbar";
+import Container from "../../components/global/Container";
 
 const Homepage = () => {
 	return (
-		<div className="h-full bg-black flex items-center justify-around flex-col">
+		<>
 			<Topbar></Topbar>
-		</div>
+			<div className="h-full bg-black pt-28">
+				<Container className="text-white">
+					<h2>Filtro de Categorias</h2>
+					<h2>Restaurantes</h2>
+					<h2>Recomendados para ti</h2>
+					<h2>Etc...</h2>
+					<hr className="py-5" />
+					<Link href="/register">Registro</Link>
+				</Container>
+			</div>
+		</>
 	);
 };
 
