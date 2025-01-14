dc2439f1a2a2cdab3a76d6c08634f48b6c59da6e
Tomas Padilla <padillatomasagustin@gmail.com>
2025-01-14 15:44:48 +0100

    [FE-15] Login Form

---
+++ app/(website)/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginPage = () => {
+	<div className="h-full flex items-center justify-around flex-col">
+		<div className="flex items-center justify-around flex-col">
+			<h1 className="text-3xl">Login page</h1>
+		</div>
+	</div>;
+};
+
+export default LoginPage;
