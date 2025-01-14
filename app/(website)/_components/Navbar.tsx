faa05f06202777ad5fa8781852ebb5ea37410a6e
Tomas Padilla <padillatomasagustin@gmail.com>
2025-01-14 20:09:48 +0100

    [FE-16] Mejorar Box Shadow

--- app/(website)/_components/Navbar.tsx
+++ app/(website)/_components/Navbar.tsx
@@ -4,7 +4,7 @@ import { House, Search, ReceiptText, User } from "lucide-react";
 
 export const Navbar = () => {
 	return (
-		<div className="absolute bottom-0 w-full py-6 px-10 bg-black text-white shadow-[0_0_0.1px_0.25px_rgba(255,255,255,0.3)]">
+		<div className="absolute bottom-0 w-full py-6 px-10 bg-black text-white shadow-[0_0.1px_0_0.25px_rgba(255,255,255,0.4)]">
 			<div className="flex h-auto text-xs text-center justify-between">
 				<Link href="/" className="flex flex-col items-center justify-center">
 					<House />
--- app/(website)/_components/Topbar.tsx
+++ app/(website)/_components/Topbar.tsx
@@ -2,7 +2,7 @@ import { Logo } from "@/components/global/Logo";
 
 export const Topbar = () => {
 	return (
-		<div className="absolute top-0 w-full py-5 px-10 bg-black shadow-[0_0_0.1px_0.25px_rgba(255,255,255,0.3)]">
+		<div className="absolute top-0 w-full py-5 px-10 bg-black shadow-[0_0.1px_1px_rgba(255,255,255,0.4)]">
 			<Logo width={100}></Logo>
 		</div>
 	);
