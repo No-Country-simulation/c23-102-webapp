65ffd6a64ac457aafacafa31b19e972808efe461
Pablo Alonso <pabloalonsonext@gmail.com>
2025-01-14 11:33:48 -0300

    [FE-14] Agregamos estilos iniciales

--- app/(website)/register/page.tsx
+++ app/(website)/register/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+
 const RegisterPage = () => {
-  return <h1>Register page</h1>;
+  return (
+    <div className="h-full flex items-center justify-around flex-col">
+			<div className="flex items-center justify-around flex-col">
+				<h1 className="text-3xl">Register page</h1>
+			</div>
+		</div>
+  )
 };
 
 export default RegisterPage;
