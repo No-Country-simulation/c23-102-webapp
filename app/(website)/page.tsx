3f1b63ebdd72a909b759e2082f3de2e00e21eb1c
Tomas Padilla <padillatomasagustin@gmail.com>
2025-01-15 18:39:55 +0100

    [FE-36] Styling Homepage

--- app/(website)/page.tsx
+++ app/(website)/page.tsx
@@ -1,31 +1,27 @@
 import React from "react";
-import Link from "next/link";
 
 import { Topbar } from "./_components/Topbar";
 import Container from "../../components/global/Container";
 import { Navbar } from "./_components/Navbar";
-import { WEBSITE_ROUTES } from "@/constants/routes";
 import { ImageCarousel } from "../../components/global/ImageCarousel";
 
 const Homepage = () => {
 	return (
 		<>
 			<Topbar></Topbar>
-			<div className="h-full bg-black pt-32">
+			<div className=" bg-black py-32">
 				<Container className="text-white">
-					<h2>Filtro de Categorias</h2>
-
-					<article>
-						<h2>Restaurantes</h2>
+					<article className="home-section">
+						<h1 className="title">Filtro Categorias</h1>
+					</article>
+					<article className="home-section">
+						<h1 className="title">Restaurantes</h1>
 						<ImageCarousel></ImageCarousel>
 					</article>
-
-					<h2>Recomendados para ti</h2>
-					<h2>Etc...</h2>
-					<br />
-					<hr />
-					<br />
-					<Link href={WEBSITE_ROUTES.REGISTER}>Registro</Link>
+					<article className="home-section">
+						<h1 className="title">Recomendados para ti</h1>
+						<ImageCarousel></ImageCarousel>
+					</article>
 				</Container>
 			</div>
 			<Navbar></Navbar>
