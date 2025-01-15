5ced1a261fd4ff8db28555af3532596aa4f331c2
Tomas Padilla <padillatomasagustin@gmail.com>
2025-01-15 18:40:50 +0100

    [FE-36] change mock data

--- constants/mock/restaurant-info.ts
+++ constants/mock/restaurant-info.ts
@@ -2,7 +2,7 @@ export const mocked_restaurants = [
 	{
 		id: 1,
 		image_url: "https://upload.wikimedia.org/wikipedia/commons/6/62/Barbieri_-_ViaSophia25668.jpg",
-		name: "Resto 1",
+		name: "Restaurante 1",
 		distance: "5 min.",
 		categories: [
 			{
@@ -18,7 +18,7 @@ export const mocked_restaurants = [
 	{
 		id: 2,
 		image_url: "https://www.geneve.com/fileadmin/_processed_/7/1/csm_starred_restaurants_2_header_05562c509f.webp",
-		name: "Resto 2",
+		name: "Restaurante 2",
 		distance: "12 min.",
 		categories: [
 			{
@@ -31,7 +31,7 @@ export const mocked_restaurants = [
 		id: 3,
 		image_url:
 			"https://images.ctfassets.net/trvmqu12jq2l/6FV4Opt7wUyR91t2FXyOIr/f32972fce10fc87585e831b334ea17ef/header.jpg?q=70&w=1208&h=1080&f=faces&fit=fill",
-		name: "Resto 3",
+		name: "Restaurante 3",
 		distance: "1 min.",
 		categories: [
 			{
@@ -43,7 +43,7 @@ export const mocked_restaurants = [
 	{
 		id: 4,
 		image_url: "https://cdn.vox-cdn.com/uploads/chorus_image/image/73039680/IMG_0725_EDIT.0.jpg",
-		name: "Resto 4",
+		name: "Restaurante 4",
 		distance: "25 min.",
 		categories: [
 			{
