9ee58f6144d31c9f022f8ac97cf914adea4e25ab
Victor <vickyflow6@gmail.com>
2025-01-17 11:07:20 -0300

    Create db.py and get_db func

---
+++ app/db.py
@@ -0,0 +1,14 @@
+"Crea y configura base de datos"
+from sqlite3 import connect, PARSE_DECLTYPES, Row
+
+from flask import current_app, g
+
+
+def get_db():
+    "Crea conexión, almacena en g.db y retorna"
+    if 'db' not in g:
+        g.db = connect(
+            current_app.config['DATABASE'], detect_types=PARSE_DECLTYPES)
+        g.db.row_factory = Row
+
+    return g.db
