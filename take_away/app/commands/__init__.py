8dd5b38d859899d927135f1a12111fc760292fb8
Victor <vickyflow6@gmail.com>
2025-01-18 17:00:12 -0300

    Modify docs of file in commands/

--- take_away/app/commands/__init__.py
+++ take_away/app/commands/__init__.py
@@ -5,6 +5,6 @@ from app.db import init_db
 
 @command('init-db')
 def init_db_command():
-    "Elimina todos los datos y crea nuevas tablas"
+    "Elimina todos los datos y crea tablas necesarias"
     init_db()
     echo('Base de datos y conexión creada')
