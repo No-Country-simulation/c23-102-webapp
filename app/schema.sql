2db0f10a5aa33e3702ea9cfcdbad106b78daa3f4
Victor <vickyflow6@gmail.com>
2025-01-20 14:16:17 -0300

    Update schema and db
    Establish primarys key as NOT NULL and AUTOINCREMENT.

--- app/schema.sql
+++ app/schema.sql
@@ -1,49 +1,38 @@
 BEGIN TRANSACTION;
-DROP TABLE IF EXISTS "Client";
-CREATE TABLE IF NOT EXISTS "Client" (
-	"Client_ID"	INTEGER,
-	"Firstname"	VARCHAR(30) NOT NULL,
-	"Lastname"	VARCHAR(30) NOT NULL,
-	"Email"	VARCHAR(30),
-	"Password"	VARCHAR(30),
-	"Phone"	VARCHAR(30),
-	"Address"	VARCHAR(50),
-	PRIMARY KEY("Client_ID")
-);
 DROP TABLE IF EXISTS "Restaurant_Category";
 CREATE TABLE IF NOT EXISTS "Restaurant_Category" (
-	"Category_ID"	INTEGER,
+	"Category_ID"	INTEGER NOT NULL,
 	"Name"	VARCHAR(30) NOT NULL,
-	PRIMARY KEY("Category_ID")
+	PRIMARY KEY("Category_ID" AUTOINCREMENT)
 );
 DROP TABLE IF EXISTS "Menu";
 CREATE TABLE IF NOT EXISTS "Menu" (
-	"Menu_ID"	INTEGER,
+	"Menu_ID"	INTEGER NOT NULL,
 	"Name"	VARCHAR(30) NOT NULL,
 	"Price"	DECIMAL(4, 1),
 	"Description"	VARCHAR(50),
 	"Availability"	BOOLEAN,
-	PRIMARY KEY("Menu_ID")
+	PRIMARY KEY("Menu_ID" AUTOINCREMENT)
 );
 DROP TABLE IF EXISTS "Order_Detail";
 CREATE TABLE IF NOT EXISTS "Order_Detail" (
-	"Order_Detail_ID"	INTEGER,
-	"Menu_ID"	INTEGER NOT NULL,
+	"Order_Detail_ID"	INTEGER NOT NULL,
+	"Menu_ID"	INTEGER,
 	"Quantity"	INTEGER,
 	"Subtotal"	DECIMAL(5, 1),
-	PRIMARY KEY("Order_Detail_ID"),
+	PRIMARY KEY("Order_Detail_ID" AUTOINCREMENT),
 	FOREIGN KEY("Menu_ID") REFERENCES "Menu"("Menu_ID")
 );
 DROP TABLE IF EXISTS "Order_Status";
 CREATE TABLE IF NOT EXISTS "Order_Status" (
-	"Order_Status_ID"	INTEGER,
+	"Order_Status_ID"	INTEGER NOT NULL,
 	"Name"	VARCHAR(30) NOT NULL,
 	"Description"	VARCHAR(50),
-	PRIMARY KEY("Order_Status_ID")
+	PRIMARY KEY("Order_Status_ID" AUTOINCREMENT)
 );
 DROP TABLE IF EXISTS "Delivery_Person";
 CREATE TABLE IF NOT EXISTS "Delivery_Person" (
-	"Delivery_Person_ID"	INTEGER,
+	"Delivery_Person_ID"	INTEGER NOT NULL,
 	"FirstName"	VARCHAR(30) NOT NULL,
 	"LastName"	VARCHAR(30),
 	"Email"	VARCHAR(30),
@@ -52,37 +41,37 @@ CREATE TABLE IF NOT EXISTS "Delivery_Person" (
 	"Address"	VARCHAR(50),
 	"Status"	BOOLEAN,
 	"Vehicle"	VARCHAR(30),
-	PRIMARY KEY("Delivery_Person_ID")
+	PRIMARY KEY("Delivery_Person_ID" AUTOINCREMENT)
 );
 DROP TABLE IF EXISTS "Payment_Type";
 CREATE TABLE IF NOT EXISTS "Payment_Type" (
-	"Payment_Type_ID"	INTEGER,
+	"Payment_Type_ID"	INTEGER NOT NULL,
 	"Name"	VARCHAR(30) NOT NULL,
 	"Description"	VARCHAR(50),
-	PRIMARY KEY("Payment_Type_ID")
+	PRIMARY KEY("Payment_Type_ID" AUTOINCREMENT)
 );
 DROP TABLE IF EXISTS "Payment_Status";
 CREATE TABLE IF NOT EXISTS "Payment_Status" (
-	"Payment_Status_ID"	INTEGER,
+	"Payment_Status_ID"	INTEGER NOT NULL,
 	"Name"	VARCHAR(30) NOT NULL,
 	"Description"	VARCHAR(50),
-	PRIMARY KEY("Payment_Status_ID")
+	PRIMARY KEY("Payment_Status_ID" AUTOINCREMENT)
 );
 DROP TABLE IF EXISTS "Payment";
 CREATE TABLE IF NOT EXISTS "Payment" (
-	"Payment_ID"	INTEGER,
+	"Payment_ID"	INTEGER NOT NULL,
 	"Payment_Type_ID"	INTEGER NOT NULL,
 	"Payment_Status_ID"	INTEGER NOT NULL,
 	"Date"	DATE,
 	"Time"	DATETIME,
 	"Reference"	VARCHAR(50),
-	PRIMARY KEY("Payment_ID"),
+	FOREIGN KEY("Payment_Status_ID") REFERENCES "Payment_Status"("Payment_Status_ID"),
 	FOREIGN KEY("Payment_Type_ID") REFERENCES "Payment_Type"("Payment_Type_ID"),
-	FOREIGN KEY("Payment_Status_ID") REFERENCES "Payment_Status"("Payment_Status_ID")
+	PRIMARY KEY("Payment_ID" AUTOINCREMENT)
 );
 DROP TABLE IF EXISTS "Order";
 CREATE TABLE IF NOT EXISTS "Order" (
-	"Order_ID"	INTEGER,
+	"Order_ID"	INTEGER NOT NULL,
 	"Client_ID"	INTEGER NOT NULL,
 	"Restaurant_ID"	INTEGER NOT NULL,
 	"Order_Detail_ID"	INTEGER NOT NULL,
@@ -93,29 +82,40 @@ CREATE TABLE IF NOT EXISTS "Order" (
 	"Time"	DATETIME,
 	"Total"	DECIMAL(5, 1),
 	"Delivery_Address"	VARCHAR(50),
-	FOREIGN KEY("Client_ID") REFERENCES "Client"("Client_ID"),
-	FOREIGN KEY("Delivery_Person_ID") REFERENCES "Delivery_Person"("Delivery_Person_ID"),
-	PRIMARY KEY("Order_ID"),
+	PRIMARY KEY("Order_ID" AUTOINCREMENT),
+	FOREIGN KEY("Order_Detail_ID") REFERENCES "Order_Detail"("Order_Detail_ID"),
+	FOREIGN KEY("Payment_ID") REFERENCES "Payment"("Payment_ID"),
 	FOREIGN KEY("Order_Status_ID") REFERENCES "Order_Status"("Order_Status_ID"),
 	FOREIGN KEY("Restaurant_ID") REFERENCES "Restaurant"("Restaurant_ID"),
-	FOREIGN KEY("Order_Detail_ID") REFERENCES "Order_Detail"("Order_Detail_ID"),
-	FOREIGN KEY("Payment_ID") REFERENCES "Payment"("Payment_ID")
+	FOREIGN KEY("Delivery_Person_ID") REFERENCES "Delivery_Person"("Delivery_Person_ID"),
+	FOREIGN KEY("Client_ID") REFERENCES "Client"("Client_ID")
+);
+DROP TABLE IF EXISTS "Client";
+CREATE TABLE IF NOT EXISTS "Client" (
+	"Client_ID"	INTEGER NOT NULL,
+	"Firstname"	VARCHAR(30) NOT NULL,
+	"Lastname"	VARCHAR(30) NOT NULL,
+	"Email"	VARCHAR(30) NOT NULL,
+	"Password"	VARCHAR(30) NOT NULL,
+	"Phone"	VARCHAR(30),
+	"Address"	VARCHAR(50),
+	PRIMARY KEY("Client_ID" AUTOINCREMENT)
 );
 DROP TABLE IF EXISTS "Restaurant";
 CREATE TABLE IF NOT EXISTS "Restaurant" (
-	"Restaurant_ID"	INTEGER,
+	"Restaurant_ID"	INTEGER NOT NULL,
 	"Category_ID"	INTEGER NOT NULL,
 	"Menu_ID"	INTEGER NOT NULL,
 	"Name"	VARCHAR(30) NOT NULL,
 	"Address"	VARCHAR(50),
 	"Description"	VARCHAR(50),
-	"Email"	VARCHAR(30),
-	"Password"	VARCHAR(30),
+	"Email"	VARCHAR(30) NOT NULL,
+	"Password"	VARCHAR(30) NOT NULL,
 	"Phone"	VARCHAR(30),
 	"Opening_Hour"	INTEGER,
 	"Closing_Hour"	INTEGER,
-	FOREIGN KEY("Category_ID") REFERENCES "Restaurant_Category"("Category_ID"),
+	PRIMARY KEY("Restaurant_ID" AUTOINCREMENT),
 	FOREIGN KEY("Menu_ID") REFERENCES "Menu"("Menu_ID"),
-	PRIMARY KEY("Restaurant_ID")
+	FOREIGN KEY("Category_ID") REFERENCES "Restaurant_Category"("Category_ID")
 );
 COMMIT;
