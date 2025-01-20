e867436738c3730a12fb0817e5eda71fb190df25
Tomas Padilla <padillatomasagustin@gmail.com>
2025-01-20 22:27:56 +0100

    [FE-43] Basic CONTEXT

--- app/(platform)/dashboard/page.tsx
+++ app/(platform)/dashboard/page.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { useUser } from "@/context/UserContext";
 
 const DashboardPage = () => {
+	const { user } = useUser();
 	return (
 		<div className="h-full flex items-center justify-around flex-col">
 			<div className="flex items-center justify-around flex-col">
 				<h1 className="text-3xl">TakeAway Dashboard</h1>
+				{user && <h2 className="text-xl">Welcome, {user.username}</h2>}
 			</div>
 		</div>
 	);
