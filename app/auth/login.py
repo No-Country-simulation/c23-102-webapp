761a17c966d28ea84550b666cf28f4b0307a8c37
Victor <vickyflow6@gmail.com>
2025-01-20 15:43:14 -0300

    Create basic structure of auth/login

---
+++ app/auth/login.py
@@ -0,0 +1,30 @@
+
+from flask import session, request, redirect, url_for
+from app import auth
+from app.db import get_db
+
+
+@auth.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
