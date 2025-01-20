947d1e52c7f4d52122d3b1efcb066e9d11bbbe11
Tomas Padilla <padillatomasagustin@gmail.com>
2025-01-20 22:02:26 +0100

    develop - quickfix container

--- components/global/Container.tsx
+++ components/global/Container.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 import React from "react";
 
 function Container({ children, className }: { children: React.ReactNode; className?: string }) {
-	return <div className={cn("mx-auto max-w-6xl xl:max-w-7xl px-8", className)}>{children}</div>;
+	return <div className={cn("center-90", className)}>{children}</div>;
 }
 
 export default Container;
