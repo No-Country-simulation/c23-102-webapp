895767a2d04e61ea6371e56290983e2b17d0dc83
Tomas Padilla <padillatomasagustin@gmail.com>
2025-01-20 21:51:07 +0100

    develop - quickfix heights

--- app/(website)/layout.tsx
+++ app/(website)/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const LandingLayout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div className="min-h-dvh">
-			<main className="h-full font-light tracking-normal">{children}</main>
+			<main className="h-dvh">{children}</main>
 		</div>
 	);
 };
