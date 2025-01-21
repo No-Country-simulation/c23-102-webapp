c80a28bb9e63cacbad58c437fb76a57a48ed9d51
Victor <vickyflow6@gmail.com>
2025-01-21 13:29:50 -0300

    Increase the validators on RegisterForm

--- app/auth/RegisterForm.py
+++ app/auth/RegisterForm.py
@@ -1,13 +1,21 @@
 """Crea validador para formulario de registro."""
 from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField
+from wtforms.fields import EmailField
+from wtforms.validators import DataRequired, Email
 
 
 class RegisterForm(FlaskForm):
     """Valida los campos del formulario de registro."""
-    name = StringField('name', validators=[DataRequired()])
+    name = StringField('name', validators=[
+        DataRequired(message="El nombre es obligatorio."),
+    ])
     lastname = StringField('lastname')
-    email = EmailField('email', validators=[DataRequired()])
-    phone = StringField('phone', validators=[DataRequired()])
-    password = PasswordField('password', validators=[DataRequired()])
+    email = EmailField('email', validators=[
+        DataRequired(message="El correo es obligatorio."),
+        Email(message="Debe ingresar un correo válido.")
+    ])
+    phone = StringField('phone')
+    password = PasswordField('password', validators=[
+        DataRequired(message="La contraseña es obligatoria."),
+    ])
