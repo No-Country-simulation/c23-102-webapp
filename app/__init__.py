2154dfe784f4a9c261f66fe7025829238e6cd59d
Victor <vickyflow6@gmail.com>
2025-01-21 14:05:14 -0300

    Deactivate CSRF protection on app

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,9 @@
 from os import makedirs
 from os.path import join
 from flask import Flask
-from .auth import auth
-
 from app.commands import init_db_command
 from app.db import close_db
+from .auth import auth
 from .restaurant import app as restaurant
 
 
@@ -15,6 +14,7 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
     app.config['DATABASE'] = join(app.instance_path, 'db.sqlite')
+    app.config['WTF_CSRF_ENABLED'] = False
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
 
