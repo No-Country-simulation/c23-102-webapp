30e7cac84c5494b5102f41dbc83f85e54169e0f5
Victor <vickyflow6@gmail.com>
2025-01-21 13:22:42 -0300

    Add fields to auth/RegisterForm

--- app/auth/RegisterForm.py
+++ app/auth/RegisterForm.py
@@ -1,9 +1,13 @@
 """Crea validador para formulario de registro."""
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import EmailField, PasswordField, StringField
 from wtforms.validators import DataRequired
 
 
 class RegisterForm(FlaskForm):
     """Valida los campos del formulario de registro."""
     name = StringField('name', validators=[DataRequired()])
+    lastname = StringField('lastname')
+    email = EmailField('email', validators=[DataRequired()])
+    phone = StringField('phone', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
