cb45e057b7e4c618cbc9f0f7dd0c48e7778599c1
Victor <vickyflow6@gmail.com>
2025-01-21 10:51:07 -0300

    Change method on auth/login and fix

--- app/auth/login.py
+++ app/auth/login.py
@@ -5,27 +5,26 @@ from app.db import get_db
 from . import auth
 
 
-@auth.route('/login', methods=('GET', 'POST'))
+@auth.post('/login')
 def login():
     """Verifica datos enviados y redirije, si todo está bien."""
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        db = get_db()
-        error = None
-        user = db.execute(
-            'SELECT * FROM User WHERE Email = ?', (email)
-        ).fetchone()
+    email = request.form['email']
+    password = request.form['password']
+    db = get_db()
+    error = None
+    user = db.execute(
+        'SELECT * FROM User WHERE Email = ?', (email,)
+    ).fetchone()
 
-        if email is None or user['Email']:
-            error = 'Email incorrecto.'
-        # Está función convierte el segundo parámetro en hash y lo compara con el primero
-        elif not check_password_hash(user['password'], password):
-            error = 'Contraseña incorrecta.'
+    if email is None or user['Email']:
+        error = 'Email incorrecto.'
+    # Está función convierte el segundo parámetro en hash y lo compara con el primero
+    elif not check_password_hash(user['password'], password):
+        error = 'Contraseña incorrecta.'
 
-        if error is not None:
-            abort(401, error)
+    if error is not None:
+        abort(401, error)
 
-        session.clear()
-        session['user_id'] = user['User_ID']
-        return redirect(url_for('index'))
+    session.clear()
+    session['user_id'] = user['User_ID']
+    return redirect(url_for('index'))
