1c013532f407a58a9385cae77dff58769c4eb1d7
Victor <vickyflow6@gmail.com>
2025-01-21 11:45:35 -0300

    Fix errors on auth/register

--- app/auth/register.py
+++ app/auth/register.py
@@ -1,33 +1,30 @@
-from flask import request
+from flask import abort, request, url_for, redirect
+from werkzeug.security import generate_password_hash
 from app.db import get_db
 from . import auth
 
 
 @auth.post('/register')
 def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
+    username = request.form['username']
+    password = request.form['password']
+    db = get_db()
+    error = None
 
-        if not username:
-            error = 'Username is required.'
-        elif not password:
-            error = 'Password is required.'
+    if not username:
+        error = 'Username is required.'
+    elif not password:
+        error = 'Password is required.'
 
-        if error is None:
-            try:
-                db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
-                )
-                db.commit()
-            except db.IntegrityError:
-                error = f"User {username} is already registered."
-            else:
-                return redirect(url_for("auth.login"))
-
-        flash(error)
-
-    return render_template('auth/register.html')
+    if error is None:
+        try:
+            db.execute(
+                "INSERT INTO user (username, password) VALUES (?, ?)",
+                (username, generate_password_hash(password)),
+            )
+            db.commit()
+        except db.IntegrityError:
+            error = f"User {username} is already registered."
+        else:
+            return redirect(url_for("auth.login"))
+    abort(401, error)
