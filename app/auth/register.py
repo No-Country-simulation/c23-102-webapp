e4fdde0e8a612769013f3ec22005f3e78324c8ec
Victor <vickyflow6@gmail.com>
2025-01-21 14:24:35 -0300

    Delete validation on register  view

--- app/auth/register.py
+++ app/auth/register.py
@@ -1,29 +1,26 @@
 """Crea vista register"""
-from flask import abort, url_for, redirect
+from flask import abort, request, url_for, redirect
 from werkzeug.security import generate_password_hash
-from app.auth.register_form import RegisterForm
 from app.db import get_db
 from . import auth
 
 
 @auth.post('/register')
 def register():
-    """Valida datos del usuario y los guarda en basa de datos."""
-    form = RegisterForm()
+    """Registra usuario en basa de datos."""
+    form = request.form
     db = get_db()
-    if form.validate_on_submit():
-        try:
-            db.execute(
-                """
-                INSERT INTO User (Email, Password, Lastname, Firstname, Phone)
-                VALUES (?, ?, ?, ?, ?)
-                """,
-                (form.email, generate_password_hash(str(form.password)),
-                 form.lastname, form.name, form.phone),
-            )
-            db.commit()
-        except db.IntegrityError:
-            abort(401, f"Usuario ya registrado con {form.email}.")
-        else:
-            return redirect(url_for(""))  # Agregar redirección a home
-    abort(401, form.errors)
+    try:
+        db.execute(
+            """
+            INSERT INTO User (Email, Password, Lastname, Firstname, Phone)
+            VALUES (?, ?, ?, ?, ?)
+            """,
+            (form['email'], generate_password_hash(form['password']),
+                form['lastname'], form['name'], form['phone']),
+        )
+        db.commit()
+    except db.IntegrityError:
+        abort(401, f"Usuario ya registrado con {form['email']}.")
+    else:
+        return redirect(url_for(""))  # Agregar redirección a home
