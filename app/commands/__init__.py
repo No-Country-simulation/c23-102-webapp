0d55db539cd7bc71f47d516b0d3bea50571a1ccf
Victor <vickyflow6@gmail.com>
2025-01-22 12:10:26 -0300

    Delete condition on init-db command

--- app/commands/__init__.py
+++ app/commands/__init__.py
@@ -1,15 +1,10 @@
 "Comandos personalizados"
-from os.path import exists
 from click import command, echo
-from flask import current_app
 from app.db import init_db
 
 
 @command('init-db')
 def init_db_command():
-    "Elimina todos los datos y crea tablas necesarias"
-    if not exists(current_app.config['DATABASE']):
-        init_db()
-        echo('Base de datos y conexión creada.')
-    else:
-        echo("La base de datos ya fue creada y conectada.")
+    "Crea base de datos."
+    init_db()
+    echo('Base de datos y conexión creada.')
