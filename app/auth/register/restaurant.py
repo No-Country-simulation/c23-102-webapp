75466c469e91a59d63535f4b58d7b6582d508b54
Victor <vickyflow6@gmail.com>
2025-01-22 12:29:41 -0300

    Add docstrings and squeleton (register/restaurant)

--- app/auth/register/restaurant.py
+++ app/auth/register/restaurant.py
@@ -1,4 +1,26 @@
+"""Crea vista 'register_restaurant'."""
+from flask import request, abort
+from werkzeug.security import generate_password_hash
+from app.db import get_db
 from . import register
 
 
 @register.post('/restaurant')
+def register_restaurant():
+    """Registra nuevo restaurant en base de datos."""
+    form = request.form
+    db = get_db()
+    try:
+        db.execute(
+            """
+            INSERT INTO User (Email, Password, Lastname, Firstname, Phone)
+            VALUES (?, ?, ?, ?, ?)
+            """,
+            (form['email'], generate_password_hash(form['password']),
+                form['lastname'], form['name'], form['phone']),
+        )
+        db.commit()
+    except db.IntegrityError:
+        abort(401, f"Usuario ya registrado con {form['email']}.")
+    else:
+        return {'email': form['email']}
