c7ec390d23af765a55999fcbfc8e0d54f95484fb
Pablo Alonso <pabloalonsonext@gmail.com>
2025-01-23 11:09:12 -0300

    [FE-34] Hicimos la RegisterPage responsiva

--- app/(website)/register/page.tsx
+++ app/(website)/register/page.tsx
@@ -5,7 +5,7 @@ import RegisterForm from "../_components/forms/RegisterForm";
 const RegisterPage = () => {
 	return (
 		<div className="h-full flex flex-col items-center justify-around bg-black text-white pt-24 pb-40">
-			<div className="flex flex-col items-center w-[85%]">
+			<div className="flex flex-col items-center  center-mobile center-forms-lg  ">
 				<Logo width={130}></Logo>
 				<RegisterForm />
 			</div>
