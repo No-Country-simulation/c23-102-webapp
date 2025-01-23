7cd5b9494b539ea1b17119621307fc49642ae192
Tomas Padilla <padillatomasagustin@gmail.com>
2025-01-23 20:51:13 +0100

    develop - fix email instead of username

--- app/(platform)/dashboard/page.tsx
+++ app/(platform)/dashboard/page.tsx
@@ -8,7 +8,7 @@ const DashboardPage = () => {
 		<div className="min-h-dvh bg-black text-white flex items-center justify-around flex-col">
 			<div className="flex items-center justify-around flex-col">
 				<h1 className="text-3xl">TakeAway Dashboard</h1>
-				{user && <h2 className="text-xl">Welcome, {user.username}</h2>}
+				{user && <h2 className="text-xl">Welcome, {user.email}</h2>}
 				<Button
 					onClick={() => {
 						logoutUser();
