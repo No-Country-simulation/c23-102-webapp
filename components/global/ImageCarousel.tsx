8bb98235271f499b3313789b22157c3adbda1aae
Tomas Padilla <padillatomasagustin@gmail.com>
2025-01-24 22:20:58 +0100

    carousel

--- components/global/ImageCarousel.tsx
+++ components/global/ImageCarousel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useMeasure } from "react-use";
 import { motion } from "framer-motion";
 import Image from "next/image";
