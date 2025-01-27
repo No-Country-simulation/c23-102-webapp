879b2cfca77dd3dff66983fa1409a0967803e6bf
Victor <vickyflow6@gmail.com>
2025-01-27 11:45:31 -0300

    Add Banner_Url to Restaurant table (schema.sql)

--- app/schema.sql
+++ app/schema.sql
@@ -14,8 +14,8 @@ CREATE TABLE IF NOT EXISTS "Order_Detail" (
 	"Menu_ID"	INTEGER,
 	"Quantity"	INTEGER,
 	"Subtotal"	DECIMAL(5, 1),
-	FOREIGN KEY("Menu_ID") REFERENCES "Menu"("Menu_ID"),
-	PRIMARY KEY("Order_Detail_ID" AUTOINCREMENT)
+	PRIMARY KEY("Order_Detail_ID" AUTOINCREMENT),
+	FOREIGN KEY("Menu_ID") REFERENCES "Menu"("Menu_ID")
 );
 DROP TABLE IF EXISTS "Order_Status";
 CREATE TABLE IF NOT EXISTS "Order_Status" (
@@ -59,9 +59,9 @@ CREATE TABLE IF NOT EXISTS "Payment" (
 	"Date"	DATE,
 	"Time"	DATETIME,
 	"Reference"	VARCHAR(50),
-	FOREIGN KEY("Payment_Type_ID") REFERENCES "Payment_Type"("Payment_Type_ID"),
 	FOREIGN KEY("Payment_Status_ID") REFERENCES "Payment_Status"("Payment_Status_ID"),
-	PRIMARY KEY("Payment_ID" AUTOINCREMENT)
+	PRIMARY KEY("Payment_ID" AUTOINCREMENT),
+	FOREIGN KEY("Payment_Type_ID") REFERENCES "Payment_Type"("Payment_Type_ID")
 );
 DROP TABLE IF EXISTS "Order";
 CREATE TABLE IF NOT EXISTS "Order" (
@@ -76,13 +76,13 @@ CREATE TABLE IF NOT EXISTS "Order" (
 	"Time"	DATETIME,
 	"Total"	DECIMAL(5, 1),
 	"Delivery_Address"	VARCHAR(50),
-	FOREIGN KEY("Restaurant_ID") REFERENCES "Restaurant"("Restaurant_ID"),
+	FOREIGN KEY("Order_Detail_ID") REFERENCES "Order_Detail"("Order_Detail_ID"),
 	FOREIGN KEY("Payment_ID") REFERENCES "Payment"("Payment_ID"),
 	FOREIGN KEY("Order_Status_ID") REFERENCES "Order_Status"("Order_Status_ID"),
-	FOREIGN KEY("Order_Detail_ID") REFERENCES "Order_Detail"("Order_Detail_ID"),
-	FOREIGN KEY("Delivery_Person_ID") REFERENCES "Delivery_Person"("Delivery_Person_ID"),
+	FOREIGN KEY("Restaurant_ID") REFERENCES "Restaurant"("Restaurant_ID"),
 	PRIMARY KEY("Order_ID" AUTOINCREMENT),
-	FOREIGN KEY("Client_ID") REFERENCES "Client"("Client_ID")
+	FOREIGN KEY("Client_ID") REFERENCES "Client"("Client_ID"),
+	FOREIGN KEY("Delivery_Person_ID") REFERENCES "Delivery_Person"("Delivery_Person_ID")
 );
 DROP TABLE IF EXISTS "User";
 CREATE TABLE IF NOT EXISTS "User" (
@@ -98,28 +98,31 @@ CREATE TABLE IF NOT EXISTS "Category" (
 	"Name"	TEXT NOT NULL,
 	PRIMARY KEY("Name")
 );
-DROP TABLE IF EXISTS "Restaurant";
-CREATE TABLE IF NOT EXISTS "Restaurant" (
-	"Opening_Hour"	TIME,
-	"Closing_Hour"	TIME,
-	"Description"	VARCHAR(100),
-	"Email"	TEXT NOT NULL,
-	"Location"	TEXT NOT NULL,
-	"Location_Name"	TEXT NOT NULL,
-	"Brand"	TEXT NOT NULL,
-	"Category_Name"	TEXT NOT NULL,
-	FOREIGN KEY("Category_Name") REFERENCES "Category"("Name"),
-	PRIMARY KEY("Email")
-);
 DROP TABLE IF EXISTS "Client";
 CREATE TABLE IF NOT EXISTS "Client" (
 	"Email"	TEXT NOT NULL,
 	"Complete_Name"	TEXT NOT NULL,
 	"Location"	TEXT,
 	"City"	TEXT,
-	PRIMARY KEY("Email"),
-	FOREIGN KEY("Email") REFERENCES "User"("Email")
+	FOREIGN KEY("Email") REFERENCES "User"("Email"),
+	PRIMARY KEY("Email")
+);
+DROP TABLE IF EXISTS "Restaurant";
+CREATE TABLE IF NOT EXISTS "Restaurant" (
+	"Opening_Hour"	TIME,
+	"Closing_Hour"	TIME,
+	"Description"	TEXT,
+	"Email"	TEXT NOT NULL,
+	"Location"	TEXT NOT NULL,
+	"Location_Name"	TEXT NOT NULL,
+	"Brand"	TEXT NOT NULL,
+	"Category_Name"	TEXT NOT NULL,
+	"Banner_Url"	TEXT,
+	FOREIGN KEY("Category_Name") REFERENCES "Category"("Name"),
+	PRIMARY KEY("Email")
 );
 INSERT INTO "Category" ("Name") VALUES ('Pizzería');
 INSERT INTO "Category" ("Name") VALUES ('Lomitería');
+INSERT INTO "Client" ("Email","Complete_Name","Location","City") VALUES ('bicho@gmail.com','Victor Ayala','Av. Espana','Ciudad del este');
+INSERT INTO "Client" ("Email","Complete_Name","Location","City") VALUES ('biho@gmail.com','Victor Ayala','Av. Espana','Ciudad del este');
 COMMIT;
