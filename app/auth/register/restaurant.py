aa6d2ece59031caaa51859500571f105843b5195
Victor <vickyflow6@gmail.com>
2025-01-27 11:43:30 -0300

    Recive file on register.restaurant endpoint

--- app/auth/register/restaurant.py
+++ app/auth/register/restaurant.py
@@ -8,33 +8,38 @@ from app.utils import allowed_file
 from . import register
 
 
-@register.post('/restaurant')
-def register_restaurant():
-    """Registra nuevo restaurant en base de datos."""
-    form = request.form
-    db = get_db()
-    try:
-        db.execute(
-            """
-            INSERT INTO Restaurant (Brand, Location, Location_Name, Category_Name, Email)
-            VALUES (?, ?, ?, ?, ?)
-            """,
-            (form['brand'], form['location'],
-             form['locationName'], form['category'], form['email'])
-        ).fetchone()
-        db.commit()
-    except db.IntegrityError:
-        abort(401, f"Restaurante ya registrado con {form['email']}.")
-    else:
-        return form
-
-
-def save_file(file: FileStorage) -> str | None:
-    """Guarda archivo en static/."""
+def save_file(file: FileStorage):
+    """Guarda archivo en static/ y retorna path."""
     allowed_extensions = ['jpg', 'png', 'jpeg']
-    if file.filename == '':
-        raise ValueError('Ningún archivo seleccionado.')
+    if not file.filename:
+        raise ValueError('Ningún archivo enviado.')
     if file.filename and allowed_file(file.filename, allowed_extensions):
         filename = secure_filename(file.filename)
         file.save(join(current_app.config['UPLOAD_FOLDER'], filename))
         return f"static/{filename}"
+
+
+@register.post('/restaurant')
+def register_restaurant():
+    """Registra nuevo restaurant en base de datos."""
+    form = request.form
+    banner_file = request.files['banner']
+    db = get_db()
+    try:
+        db.execute(
+            """
+            INSERT INTO Restaurant (Brand, Location, Location_Name, Category_Name, Email, Banner_Url)
+            VALUES (?, ?, ?, ?, ?, ?)
+            """,
+            (form['brand'], form['location'],
+             form['locationName'], form['category'], form['email'], save_file(
+                 banner_file))
+        ).fetchone()
+        db.commit()
+    except db.IntegrityError:
+        abort(401, f"Restaurante ya registrado con {form['email']}.")
+    except ValueError as error:
+        error_message = str(error)
+        abort(401, error_message)
+    else:
+        return form
