32e312d237847f21ce9ffb8982da4d6668ad12bd
Victor <vickyflow6@gmail.com>
2025-01-27 11:27:17 -0300

    Improve docstrings on utils

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,7 +1,16 @@
 """Almacena funciones puras"""
 
 
-def allowed_file(filename, allowed_extensions):
-    """Verifica que el tipo de un archivo sea el esperado."""
+from typing import List
+
+
+def allowed_file(filename: str, allowed_extensions: List[str]):
+    """Verifica que el tipo de un archivo sea el esperado.
+
+    Examples
+    ________
+    >>> allowed_file('messi.png', ['png'])
+    True
+    """
     file_extension = filename.rsplit('.', 1)[1].lower()
     return '.' in filename and file_extension in allowed_extensions
