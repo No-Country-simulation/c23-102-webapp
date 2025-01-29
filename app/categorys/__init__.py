8c5efc96f3e66a3485eeec696c11917470d17167
Victor <vickyflow6@gmail.com>
2025-01-29 13:06:45 -0300

    Use registers_to_dict on categorys endpoint

--- app/categorys/__init__.py
+++ app/categorys/__init__.py
@@ -1,6 +1,7 @@
 """Crea Blueprint y endpoint categorys."""
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from app.db import get_db
+from app.utils import registers_to_list
 
 
 categorys_bp = Blueprint('categorys', __name__, url_prefix='/categorys')
@@ -12,8 +13,8 @@ def categorys():
     db = get_db()
 
     # try:
-    category_registers = db.execute("SELECT * FROM Category")
+    registers = db.execute("SELECT * FROM Category").fetchall()
 
-    formatted_restaurant_info = format_restaurant_info(restaurant_info)
+    registers_list = registers_to_list(registers)
 
-    return jsonify(formatted_restaurant_info)
+    return jsonify(registers_list)
