0bc76811d1c3b39ea166a595d00e12159fc138a9
Victor <vickyflow6@gmail.com>
2025-01-29 13:56:22 -0300

    Improve exception handle on payments blueprint

--- app/payments/__init__.py
+++ app/payments/__init__.py
@@ -7,14 +7,14 @@ payments_bp = Blueprint('payments', __name__, url_prefix='/payments')
 stripe.api_key = 'sk_test_51QkX9wAnZzuVM90TT8EtJ0KkD1zCjTPvVMgY8vWUgGCws7sMi6OKaHPjzOlQpDBYbspqqWMNAoNdpR2qmNuihF7a00HLswtzWc'
 
 
-@payments_bp.route('/create-checkout-session/<id>')
-def create_checkout_session(id: str):
+@payments_bp.route('/create-checkout-session/<price_id>')
+def create_checkout_session(price_id: str):
     """Crea una nueva pasarela de pago y redirje a ella."""
     try:
         checkout_session = stripe.checkout.Session.create(
             line_items=[
                 {
-                    'price': id,
+                    'price': price_id,
                     'quantity': 1,
                 },
             ],
@@ -22,7 +22,8 @@ def create_checkout_session(id: str):
             success_url=YOUR_DOMAIN + '/success.html',
             cancel_url=YOUR_DOMAIN + '/cancel.html',
         )
-    except Exception as e:
-        return str(e)
+    except stripe.InvalidRequestError as error:
+        error_message = str(error)
+        return str(error_message)
 
     return redirect(checkout_session['url'], code=303)
