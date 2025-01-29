f83e441cafae583ef8eefd0a9d406b4215e0df99
Victor <vickyflow6@gmail.com>
2025-01-29 13:33:52 -0300

    Crea payments blueprint and use stripe on

---
+++ app/payments/__init__.py
@@ -0,0 +1,26 @@
+from flask import Blueprint
+import stripe
+
+payments_bp = Blueprint('payments', __name__, url_prefix='/payments')
+
+stripe.api_key = 'sk_test_51QkX9wAnZzuVM90TT8EtJ0KkD1zCjTPvVMgY8vWUgGCws7sMi6OKaHPjzOlQpDBYbspqqWMNAoNdpR2qmNuihF7a00HLswtzWc'
+
+
+@payments_bp.route('/create-checkout-session', methods=['POST'])
+def create_checkout_session():
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    'price': 'price_1QkXJKAnZzuVM90TeqMNDb6G',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/success.html',
+            cancel_url=YOUR_DOMAIN + '/cancel.html',
+        )
+    except Exception as e:
+        return str(e)
+
+    return redirect(checkout_session.url, code=303)
