3a416ad1b44a500bfa38269c24c4390077e9de21
Tomas Padilla <padillatomasagustin@gmail.com>
2025-01-29 14:48:01 +0100

    [FE-59] Fix mocks

--- app/(restaurant-platform)/dashboard/restaurant-platos/page.tsx
+++ app/(restaurant-platform)/dashboard/restaurant-platos/page.tsx
@@ -11,7 +11,7 @@ const RestaurantPlatosPage = () => {
 	if (user)
 		return (
 			<div className="bg-black text-white flex items-center justify-around flex-col">
-				<div className="flex items-center justify-around flex-col">
+				<div className="flex items-center justify-around flex-col gap-8">
 					<h1 className="text-3xl"> Platos </h1>
 					{!platos && <LinkButton route={RESTAURANT_ROUTES.RESTAURANT_PLATOS_CREATE}>Create</LinkButton>}
 				</div>
--- constants/mock/authentication.ts
+++ constants/mock/authentication.ts
@@ -29,6 +29,7 @@ export const loggedRestaurantDetails: RestaurantDetailsType = {
 			description: "Con de todo",
 			image_url: "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/cf29c385555335.5d7fb3c164383.jpg",
 			price: "15.50",
+			disponible: true,
 		},
 		{
 			id: "2",
@@ -36,6 +37,7 @@ export const loggedRestaurantDetails: RestaurantDetailsType = {
 			description: "Con de todo 2",
 			image_url: "https://www.carnicerosdenavarra.com/wp-content/uploads/2023/02/smashburguer.jpg",
 			price: "18",
+			disponible: true,
 		},
 		{
 			id: "3",
@@ -43,6 +45,7 @@ export const loggedRestaurantDetails: RestaurantDetailsType = {
 			description: "Con de todo 3",
 			image_url: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/27/ea/5b/a0/combo-de-hamburguesa.jpg",
 			price: "22",
+			disponible: true,
 		},
 	],
 };
