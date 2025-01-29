56d8594a3ebda6b7a54221e3824d8a5885c25006
Victor <vickyflow6@gmail.com>
2025-01-29 12:17:58 -0300

    Execute a consult and add docstring (categorys)

--- app/categorys/__init__.py
+++ app/categorys/__init__.py
@@ -1,7 +1,19 @@
 """Crea Blueprint y endpoint categorys."""
 from flask import Blueprint
+from app.db import get_db
 
 
-categorys = Blueprint('categorys', __name__, url_prefix='/categorys')
+categorys_bp = Blueprint('categorys', __name__, url_prefix='/categorys')
 
-categorys.get('/')
+
+@categorys_bp.get('/')
+def categorys():
+    """Recupera lista de categorias de la base de datos y retorna."""
+    db = get_db()
+
+    # try:
+    category_registers = db.execute("SELECT * FROM Category")
+
+    formatted_restaurant_info = format_restaurant_info(restaurant_info)
+
+    return jsonify(formatted_restaurant_info)
