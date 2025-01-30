d3cee5a95c8136a71ca77a9407cf42db26f94693
Victor <vickyflow6@gmail.com>
2025-01-30 14:11:53 -0300

    Add docstrings to edit endpoint

--- app/profile/edit.py
+++ app/profile/edit.py
@@ -1,1 +1,9 @@
+"""Crea edit endponit."""
+from flask import request
 from . import profile
+
+
+@profile.put('/edit/<email>')
+def edit(email: str):
+    """Edita datos del cliente, o restaurante, en base de datos."""
+    data = request.get_json()
