8b5f85325af26c25c5380c250ba1691a8fbf5262
Victor <vickyflow6@gmail.com>
2025-01-30 13:56:34 -0300

    Save photo on register_client

--- app/auth/register/client.py
+++ app/auth/register/client.py
@@ -1,6 +1,7 @@
 """Crea vista 'register_client'."""
 from flask import request, abort
 from app.db import get_db
+from app.utils import save_image
 from . import register
 
 
@@ -8,14 +9,17 @@ from . import register
 def register_client():
     """Registra nuevo client en base de datos."""
     form = request.form
+    photo = request.files['photo']
+
     db = get_db()
     try:
         db.execute(
             """
-            INSERT INTO Client (Email, Location, Postal_Code)
-            VALUES (?, ?, ?)
+            INSERT INTO Client (Email, Location, Postal_Code, Photo_Path)
+            VALUES (?, ?, ?, ?)
             """,
-            (form['email'], form['location'], form['postalCode'])
+            (form['email'], form['location'],
+             form['postalCode'], save_image(photo))
         ).fetchone()
         db.commit()
     except db.IntegrityError:
