eb091a5e37764e16f58b5fc9a615ebf7c2269269
Victor <vickyflow6@gmail.com>
2025-01-30 15:08:42 -0300

    Fix sintax erros on edit endpoint

--- app/profile/edit.py
+++ app/profile/edit.py
@@ -1,6 +1,8 @@
 """Crea edit endponit."""
-from flask import request
+from logging import error
+from flask import abort, jsonify, request
 
+from app.db import get_db
 from app.utils import save_image
 from app.utils.get_restaurant_data import get_restaurant_data
 from app.utils.get_user_data import get_user_data
@@ -12,9 +14,9 @@ def edit(email: str):
     """Edita datos del cliente, o restaurante, en base de datos."""
     data = request.get_json()
     user_data = get_user_data(email)
-    banner = request.files['banner']
+    db = get_db()
 
-    if user_data['Role'] == 'Restaurante':
+    if user_data['Role_Name'] == 'Restaurante':
         restaurant_data = get_restaurant_data(email)
         query = """
         UPDATE Restaurant
@@ -25,17 +27,26 @@ def edit(email: str):
         Location_Name = ?,
         Brand = ?,
         Category_Name = ?,
-        Banner_Url = ?
         WHERE Email = ?
         """
-        opening_hour = data.get('openingHour', restaurant_data['opening_hour'])
-        closing_hour = data.get('closingHour', restaurant_data['closing_hour'])
-        description = data.get('description', restaurant_data['description'])
-        location = data.get('location', restaurant_data['location'])
+
+        opening_hour = data.get('openingHour', restaurant_data['Opening_Hour'])
+        closing_hour = data.get('closingHour', restaurant_data['Closing_Hour'])
+        description = data.get('description', restaurant_data['Description'])
+        location = data.get('location', restaurant_data['Location'])
         location_name = data.get(
-            'locationName', restaurant_data['location_name'])
-        brand = data.get('brand', restaurant_data['brand'])
+            'locationName', restaurant_data['Location_Name'])
+        brand = data.get('brand', restaurant_data['Brand'])
         category_name = data.get(
-            'categoryName', restaurant_data['category_name'])
-        banner_url = data.get(save_image(
-            banner), restaurant_data['banner_url'])
+            'categoryName', restaurant_data['Category_Name'])
+
+        try:
+            db.execute(query, (opening_hour, closing_hour, description,
+                               location, location_name, brand, category_name))
+        except Exception as e:
+            error_message = str(e)
+            print(error_message)
+        else:
+            update_data = get_restaurant_data(email)
+            return jsonify(update_data)
+    return 400
