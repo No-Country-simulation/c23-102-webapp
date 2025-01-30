c4ca8649c16da92c9a2921d0101162a6a31e9b98
Victor <vickyflow6@gmail.com>
2025-01-30 13:14:46 -0300

    Return data formatted form payments.history

--- app/payments/history.py
+++ app/payments/history.py
@@ -1,7 +1,16 @@
 """Crea history endpoint."""
+from app.db import get_db
+from app.utils import registers_to_list
 from . import payments_bp
 
 
 @payments_bp.post('/history/<email>')
 def history(email: str):
     """Recupera historial de pagos relacionados con el parámetro pasado (email)."""
+    db = get_db()
+    query = """SELECT * FROM Payment WHERE Restaurante_Email = ? OR Client_Email = ?"""
+
+    data = db.execute(query, (email,)).fetchall()
+    data_formatted = registers_to_list(data)
+
+    return data_formatted
