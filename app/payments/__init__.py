9a1dee028a33cc40807d2f47c15afe57e5cf4296
Victor <vickyflow6@gmail.com>
2025-01-30 13:15:33 -0300

    Use history enpoint on payments

--- app/payments/__init__.py
+++ app/payments/__init__.py
@@ -3,4 +3,4 @@ from flask import Blueprint
 
 payments_bp = Blueprint('payments', __name__, url_prefix='/payments')
 
-from . import create_checkout_session
+from . import create_checkout_session, history
