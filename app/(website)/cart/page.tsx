6dbd9b6c35f01d15617684aa915bff8d64f03d1f
Tomas Padilla <padillatomasagustin@gmail.com>
2025-01-31 18:16:09 +0100

    develop - Cart Styles FALTA DESKTOP

--- app/(website)/cart/page.tsx
+++ app/(website)/cart/page.tsx
@@ -16,7 +16,7 @@ const CartPage = () => {
 					<h2 className="font-semibold">En Proceso</h2>
 					<span className=" border-b-[color:--primary-color] border-b-2 w-[8rem]"></span>
 				</div>
-				<div className="flex flex-col gap-12 items-center justify-between">
+				<div className="flex flex-col gap-[2.5rem] items-center justify-between">
 					{Object.entries(restaurantSubtotals).map(([locationName, subtotal]) => (
 						<article key={locationName} className="flex flex-col gap-4">
 							<h2 className="font-bold text-2xl">{locationName}</h2>
@@ -25,7 +25,7 @@ const CartPage = () => {
 								.map(({ id, product, quantity }) => (
 									<CartItemListCard key={id} id={id} product={product} quantity={quantity} />
 								))}
-							<div className="flex flex-col gap-1 mt-[0.17rem] opacity-60">
+							<div className="flex flex-col gap-1 mt-[0.17rem] opacity-40">
 								<div className="flex flex-row items-center justify-between w-[98%] m-auto">
 									<h2 className="font-normal">Subtotal</h2>
 									<p>{"$ " + subtotal.toFixed(2)}</p>
