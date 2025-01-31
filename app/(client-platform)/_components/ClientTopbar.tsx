1c6f14e71c28b62989f6b7be07f333781cd60a0f
Tomas Padilla <padillatomasagustin@gmail.com>
2025-01-31 11:39:51 +0100

    [FE-65] Starting Page, fixed TOPBAR

--- app/(client-platform)/_components/ClientTopbar.tsx
+++ app/(client-platform)/_components/ClientTopbar.tsx
@@ -13,7 +13,7 @@ export const ClientTopbar = () => {
 		return (
 			<>
 				{/* Mobile */}
-				<div className="absolute top-0 bg-black shadow-[0_0.1px_1px_rgba(255,255,255,0.4)] py-3 pt-6 base-top-styles base-top-styles-lg">
+				<div className="absolute top-0 bg-black shadow-[0_0.1px_1px_rgba(255,255,255,0.4)] py-3 pt-6 base-top-styles base-top-styles-lg z-50">
 					<div className="flex justify-between items-center w-10/12 xl:w-[85%]">
 						<Logo width={85} className={"lg:w-28"}></Logo>
 						<div className="flex text-white gap-3 self-end opacity-[0.9] lg:self-center lg:gap-7 lg:items-center">
--- app/(website)/restaurant-details/[id]/page.tsx
+++ app/(website)/restaurant-details/[id]/page.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import Container from "@/components/global/Container";
 import { restaurantDetails } from "@/actions/restaurantsActions";
+import Image from "next/image";
 
 const RestaurantDetailsPage = async ({ params }: { params: Promise<{ id: string }> }) => {
 	const { id } = await params;
-	const restaurant = await restaurantDetails(id);
+	const { brand, category, description, email, image_url, lastName, name, location, locationName, phone, platos } =
+		await restaurantDetails(id);
 
 	return (
-		<div className="min-h-dvh escape-navbar escape-footer">
+		<div className="min-h-dvh escape-navbar escape-footer relative">
+			<Image
+				src={image_url}
+				alt="Selected Restaurant"
+				width={0}
+				height={0}
+				className="w-full h-[68vh] object-cover pt-2"
+			></Image>
 			<Container size="center-container-sm" className="flex flex-col items-center gap-8 pt-10">
-				<h2>Detalles del Restaurante por ID</h2>
-				<h2>{restaurant.locationName}</h2>
+				<h2>Hola</h2>
 			</Container>
 		</div>
 	);
