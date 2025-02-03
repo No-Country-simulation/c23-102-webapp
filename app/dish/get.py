a354ec30f4624041e82e3bfe46f2787d79b24909
Victor <vickyflow6@gmail.com>
2025-02-03 15:59:53 -0300

    Handle bad request on dish/get

--- app/dish/get.py
+++ app/dish/get.py
@@ -1,4 +1,5 @@
 """Crea get_dish vista."""
+from flask import abort
 from app.db import get_db
 from app.utils import register_to_dict
 from . import dish
@@ -10,8 +11,11 @@ def get_dish(price_id: str):
     db = get_db()
 
     query = "SELECT * FROM Dish WHERE Price_ID = ?"
-
     data = db.execute(query, (price_id,)).fetchone()
+
+    if not data:
+        abort(400, "No existe platillo relacionado con ese id.")
+
     formatted_data = register_to_dict(data)
 
     return formatted_data
